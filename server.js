97856e37a558a2d9e9d08f31ba80aee8d73a05e5
jepoy92 <jeffrey1choi@gmail.com>
2020-11-16 01:54:15 -0800

    added service-worker.js file and re-added webmanifest. having issues with heroku deploy

--- server.js
+++ server.js
@@ -3,23 +3,26 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
-
-app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", 
+{
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
+  useCreateIndex: true,
+  useFindAndModify: false
 });
+
+mongoose.connection.on('connected', () => {
+  console.log("Mongoose is connected!");
+})
 
 // routes
 app.use(require("./routes/api.js"));
